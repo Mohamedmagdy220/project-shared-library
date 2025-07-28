bd6492609f5397425d407c72c590f4559654ae8c
Mohamedmagdy220 <mohamedelngar062@gmail.com>
2025-07-28 23:43:28 +0000

    editing vars

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "Building Docker image..."   
-    sh "cd docker/blog/
-	docker build -t $IMAGE_NAME:${env.BUILD_NUMBER} ."
+    sh """ cd ./docker/blog/
+	   docker build -t $IMAGE_NAME:${env.BUILD_NUMBER} ."""
     echo "Docker image built successfully!"
 }
