48da86b17e534d319b67b9a99e325f77268388f3
Mohamedmagdy220 <mohamedelngar062@gmail.com>
2025-07-29 01:14:47 +0000

    edit vars

--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -3,8 +3,8 @@ def call() {
 
     def deploymentFile = readFile('k8s/app-deployment.yml')
 
-    def newImageTag = "my-app:${env.BUILD_NUMBER}"
-    deploymentFile = deploymentFile.replaceAll("my-app:.*", "my-app:${newImageTag}")
+    def newImageTag = "$IMAGE_NAME:${env.BUILD_NUMBER}"
+    deploymentFile = deploymentFile.replaceAll("$IMAGE_NAME:.*", "$IMAGE_NAME:${newImageTag}")
 
     writeFile(file: 'k8s/app-deployment.yml', text: deploymentFile)
 
