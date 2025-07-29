bb1af1835947217043bc189b6bc0948e8ac338c7
Mohamedmagdy220 <mohamedelngar062@gmail.com>
2025-07-29 02:27:24 +0000

    edit vars

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -5,7 +5,9 @@ def call() {
     sh 'git config --global user.name "Jenkins"'
     sh 'git config --global user.email "jenkins@example.com"'
 
-
+    withCredentials([usernamePassword(credentialsId: DOCKERHUB_CREDENTIALS, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                        echo "Logging into DockerHub..."
+                        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
     sh '''
 	git checkout main
 
