6288198ede41887ed10d2cf3dc4b24555f9f5bb0
Mohamedmagdy220 <mohamedelngar062@gmail.com>
2025-07-29 00:06:20 +0000

    editing vars

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,4 +1,9 @@
-def call(){
-	echo "scanning docker image..."
-	sh "trivy image --exit-code 0 $IMAGE_NAME:${env.BUILD_NUMBER} "
+def call() {
+    echo "🛡️ Starting Trivy scan for image: $IMAGE_NAME:${env.BUILD_NUMBER}"
+    sh '''
+        echo "🔍 Running Trivy Scan..."
+        trivy image --exit-code 0 --no-progress $IMAGE_NAME:${BUILD_NUMBER} || true
+        echo "✅ Trivy scan completed."
+	echo "🔁 Continuing pipeline after scan"
+    '''
 }
